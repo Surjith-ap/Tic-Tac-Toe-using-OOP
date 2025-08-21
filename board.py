fb6ec45715246b4922d0b1261c2c74aca7e14cd0
wizdeX <surjith.ap007@gmail.com>
2025-08-21 11:47:58 +0530

    implemented game over logic

--- board.py
+++ board.py
@@ -40,6 +40,35 @@ class Board:
             print("Enter at a different position")
 
     def check_game_over(self, player, last_move):
-        return
+        return ((self.check_row(player, last_move))
+                or (self.check_column(player, last_move))
+                or (self.check_diagonal(player))
+                or (self.check_antidiagonal(player))) # using or because wither one of the values is true returns true else returns false
 
+    def check_row(self,player,last_move):
+        row_index = last_move.get_row()
+        board_row = self.game_board[row_index]
+        return board_row.count(player.marker) == 3
 
+    def check_column(self, player, last_move):
+        markers_count = 0
+        column_index = last_move.get_column()
+        for i in range(3):
+            if self.game_board[i][column_index] == player.marker:
+                markers_count += 1
+        return markers_count == 3
+
+    def check_diagonal(self, player):
+        markers_count = 0
+        for i in range(3):
+            if self.game_board[i][i] == player.marker:
+                markers_count += 1
+        return markers_count == 3
+
+    def check_antidiagonal(self, player):
+        markers_count = 0
+        for i in range(3):
+            if self.game_board[i][2-i] == player.marker:
+                markers_count += 1
+        return markers_count == 3
+    
